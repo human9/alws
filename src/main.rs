f033ea3f9ca9b9f664bfb1a3f8a641a2f5007ab4
John Salamon <john.salamon@sahmri000435.local>
2018-03-06 16:54:06 +1030

    nice structure

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use chrono::prelude::*;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
+extern crate serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Mission {
@@ -17,13 +18,36 @@ struct Mission {
     completion: Option<MissionEntry>,
 }
 
+impl Mission {
+    pub fn new(title: String, description: String) -> Self {
+        Mission {
+            title,
+            description,
+            timestamp: Utc::now(),
+            entries: Vec::new(),
+            completion: None,
+        }
+    }
+
+    pub fn add_entry(&mut self, entry_text: String) {
+        let entry = MissionEntry {
+            timestamp: Utc::now(),
+            entry_text,
+        };
+        self.entries.push(entry); 
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct MissionEntry {
     timestamp: DateTime<Utc>,
-    entrytext: String,
+    entry_text: String,
 }
 
 fn main() {
     let mut graph = Graph::<Mission, Option<String>>::new();
-    println!("{:?}", graph);
+    let mut mission = Mission::new("A Life Well Spent".to_string(), "The winds of a new beginning blow".to_string());
+    mission.add_entry("Something happened on this date".to_string());
+    graph.add_node(mission);
+    println!("{}", serde_json::to_string_pretty(&graph).unwrap());
 }

c5b4521142aa6e368fb9efb81f0973615ed11233
John Salamon <john.salamon@sahmri000435.local>
2018-03-07 11:40:58 +1030

    base done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::path::PathBuf;
+use std::env;
+
 extern crate petgraph;
 use petgraph::Graph;
 
@@ -7,6 +14,7 @@ use chrono::prelude::*;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
+use serde::de::DeserializeOwned;
 extern crate serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -45,9 +53,50 @@ struct MissionEntry {
 }
 
 fn main() {
+
+    let path = default_path();
+    let file = open_file(&path);
+    let log = open_log(&file);
+
+    write_to_file(&path, &log);
+}
+
+fn open_log(file: &File) -> Graph<Mission, Option<String>> {
+    let graph = match serde_json::from_reader(file) {
+        Ok(graph) => graph,
+        Err(_) => create_log(),
+    };
+    graph
+}
+
+fn open_file(path: &PathBuf) -> File {
+    match File::open(path) {
+        Err(_) => match File::create(&path) {
+            Ok(file) => file,
+            Err(why) => panic!("failed to create or open {}: {}", path.display(), why.description()),
+        }
+        Ok(file) => file,
+    }
+}
+
+fn default_path() -> PathBuf {
+    let mut path = match env::home_dir() {
+        Some(path) => path,
+        None => panic!("Failed to find home dir!"),
+    };
+    path.push(".alws.json");
+    path
+}
+
+fn write_to_file(path: &PathBuf, log: &Graph<Mission, Option<String>>) {
+    let file = File::create(path).unwrap();
+    serde_json::to_writer_pretty(&file, log).unwrap();
+}
+
+fn create_log() -> Graph<Mission, Option<String>> {
     let mut graph = Graph::<Mission, Option<String>>::new();
     let mut mission = Mission::new("A Life Well Spent".to_string(), "The winds of a new beginning blow".to_string());
     mission.add_entry("Something happened on this date".to_string());
     graph.add_node(mission);
-    println!("{}", serde_json::to_string_pretty(&graph).unwrap());
+    graph
 }

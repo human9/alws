5e6b3c97503a895610cb51fed50f9aedd350fb5e
John Salamon <john.salamon@sahmri000435.local>
2018-03-10 14:18:26 +1030

    v basic editing and stuff

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,7 @@
+extern crate ncurses;
+use ncurses::*;
+
+use view::LogView;
 /// Simple text buffer
 pub struct Buffer {
 	lines: Vec<String>,
@@ -9,6 +13,28 @@ impl Buffer {
 		Buffer {
 			lines: Vec::new(),
 		}
+	}
+
+	pub fn capture_input(&self, lv: &mut LogView) {
+
+		let mut ch = getch();
+		while ch != 10 {
+			match ch {
+				_ => {
+					let index = item_index(current_item(lv.menu)) as usize;
+	                {
+	                	let ei = {
+	                    	let ref mut mission = lv.log.mission_list()[index];
+	                    	mission.entries.len()-1
+	                    };
+	                   lv.log.mission_list()[index].entries[ei].entry_text.push(ch as u8 as char);
+	                }
+					lv.draw_window();
+				},
+			}
+			ch = getch();
+		}
+
 	}
 
 }
